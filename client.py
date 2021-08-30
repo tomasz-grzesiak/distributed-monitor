effa636b187dfbb23d2e6c1802ced52c02e35461
Tomasz Grzesiak <tom.grzes3141@gmail.com>
2021-08-30 23:15:27 +0200

    Wiadomości o dołączeniu nowego węzła do systemu

--- client.py
+++ client.py
@@ -3,16 +3,48 @@
 import zmq
 import resources.message_pb2 as protobuf
 
-context = zmq.Context()
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
 
-request = protobuf.InitRequestMessage()
-request.address = "tcp://localhost:5555"
-request.ready = True
-print(request)
-socket.send(request.SerializeToString())
+def set_up_port_mapper_zmq(context):
+    own_socket = context.socket(zmq.PUB)
+    own_port = own_socket.bind_to_random_port('tcp://*')    # own socket for communication with other processes
 
-response = protobuf.InitResponseMessage()
-response.ParseFromString(socket.recv())
-print(response)
+    port_mapper_socket = context.socket(zmq.REQ)
+    port_mapper_socket.connect('tcp://localhost:5555')  # connection with portMapper
+
+    return [port_mapper_socket, own_socket, own_port]
+
+
+def set_up_node_zmq(address, context):
+    print(address)
+    socket = context.socket(zmq.SUB)
+    socket.connect(address)
+    return socket
+
+
+def build_init_request(port: int, ready: bool):
+    request = protobuf.InitRequestMessage()
+    request.address = f'tcp://localhost:{port}'
+    request.ready = ready
+    return request
+
+
+def main():
+    context = zmq.Context()
+    [port_mapper_socket, own_socket, own_port] = set_up_port_mapper_zmq(context)
+    request = build_init_request(own_port, False)
+    print(request)
+    port_mapper_socket.send(request.SerializeToString())
+
+    response = protobuf.InitResponseMessage()
+    response.ParseFromString(port_mapper_socket.recv())
+    print(response)
+
+    node_sockets = [set_up_node_zmq(address, context) for address in response.addresses if str(own_port) not in address]
+
+    request = build_init_request(own_port, True)
+    print(request)
+    port_mapper_socket.send(request.SerializeToString())
+
+
+if __name__ == '__main__':
+    main()
--- port-mapper.py
+++ port-mapper.py
@@ -3,18 +3,56 @@
 import zmq
 import resources.message_pb2 as protobuf
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
 
-# while True:
-request = protobuf.InitRequestMessage()
-request.ParseFromString(socket.recv())
-print("Received request: %s" % request)
-response = protobuf.InitResponseMessage()
-response.processID = 1;
-response.portMapperAddress = '111.222.111.222'
-response.addresses.append('1.2.3.4')
-response.addresses.append('2.3.4.5')
-print("Sending response: %s" % response)
-socket.send(response.SerializeToString())
+processes = {}
+max_process_number = 0
+pub_address = 'tcp://*:5566'
+
+
+def set_up_zmq():
+    context = zmq.Context()
+    rep_socket = context.socket(zmq.REP)
+    rep_socket.bind("tcp://*:5555")
+
+    pub_socket = context.socket(zmq.PUB)
+    pub_socket.bind(pub_address)
+    return [rep_socket, pub_socket]
+
+
+def read_init_request(socket):
+    request = protobuf.InitRequestMessage()
+    request.ParseFromString(socket.recv())
+    return request
+
+
+def build_init_response():
+    response = protobuf.InitResponseMessage()
+    response.processID = max_process_number
+    response.portMapperAddress = pub_address
+    for node_address in processes.values():
+        response.addresses.append(node_address)
+    return response
+
+
+def main():
+    [rep_socket, pub_socket] = set_up_zmq()
+    while True:
+        request = read_init_request(rep_socket)
+        print('Received request: %s' % request)
+        global max_process_number
+        max_process_number = max_process_number + 1
+        processes[max_process_number] = request.address
+
+        response = build_init_response()
+        print('Sending response: %s' % response)
+        rep_socket.send(response.SerializeToString())
+
+        ready_request = read_init_request(rep_socket)
+        if ready_request.address != request.address:
+            print('Source of messages does not match, request=%s, ready_request=%s'
+                  % request.address, ready_request.address)
+            return
+
+
+if __name__ == '__main__':
+    main()
